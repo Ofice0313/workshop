427a9fe6f77a01637e453efc7c147b01cea17908
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-03-04 02:03:15 +0200

    User entity and resource

--- src/main/java/com/devcaleb/course/application/CourseApplication.java
+++ src/main/java/com/devcaleb/course/application/CourseApplication.java
@@ -1,4 +1,4 @@
-package com.devcaleb.course;
+package com.devcaleb.course.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
